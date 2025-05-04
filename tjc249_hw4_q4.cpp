d2706666ab595cc4e27c759fca4a8b7e1d761ad0
tcampbell01 <teresa.j.campbell@gmail.com>
2025-05-03 21:29:08 -0400

    question 4 done

--- tjc249_hw4_q4.cpp
+++ tjc249_hw4_q4.cpp
@@ -6,6 +6,9 @@ using namespace std;
 
 int main() {
 
+
+    cout << "Section A below: " << endl;
+
     int length;
     int sequence;
     int i;
@@ -23,11 +26,40 @@ int main() {
         
     }
 
-    // cout << "base = " << base;
 
     double root = 1.0/(double)length;
 
-    cout << "The root is: " << root << endl;
+
+     geometric_mean = pow(base, root);
+
+     std::cout << std::fixed << std::setprecision(4) << "The geometric root is: " << geometric_mean << endl;
+
+
+     cout << "Section B below: " << endl;
+
+    int sequence = 0;
+    double geometric_mean;
+    int base = 1;
+    int counter = 0;
+
+    cout << "Please enter a non-empty sequence of positive integers, each one in a separate line. End your sequence by typing -1:" << endl;
+   
+
+
+    while (sequence != -1) {
+        cin >> sequence;
+        
+        if (sequence != -1) {
+            counter += 1;
+            base = base * sequence;
+        }
+        continue;
+        
+    }
+
+
+    double root = 1.0/(double)counter;
+
 
      geometric_mean = pow(base, root);
 
